0cf2e2417806e15576ec7fca3ab488840e74f4a6
Murielle17 <66876243+Murielle17@users.noreply.github.com>
2024-11-18 22:19:12 +0400

    Fix incorrect entry point in setup.py

--- setup.py
+++ setup.py
@@ -1,39 +1,47 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
+# Obtenez le chemin absolu du répertoire actuel
 here = path.abspath(path.dirname(__file__))
 
-with open("README.md") as f:
+# Lire le fichier README.md pour la description longue
+with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
+# Lire la version depuis "__init__.py" dans le dossier emv
 version = {}
-with open(path.join(here, "emv", "__init__.py")) as fp:
+with open(path.join(here, "emv", "__init__.py"), encoding="utf-8") as fp:
     exec(fp.read(), version)
 
+# Configuration du package
 setup(
-    name="emv",
-    version=version["__version__"],
-    description="EMV Smartcard Protocol Library",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="MIT",
-    author="Russ Garrett",
-    author_email="russ@garrett.co.uk",
-    url="https://github.com/russss/python-emv",
-    classifiers=[
+    name="emv.MurielleM",  # Nom de votre package
+    version=version["__version__"],  # Version extraite de __init__.py
+    description="EMV Smartcard Protocol Library",  # Description courte
+    long_description=long_description,  # Description longue
+    long_description_content_type="text/markdown",  # Type de contenu pour la description longue
+    license="MIT",  # Licence
+    author="Russ Garrett",  # Auteur
+    author_email="russ@garrett.co.uk",  # Email de l'auteur
+    url="https://github.com/russss/python-emv",  # URL du projet
+    classifiers=[  # Liste des classificateurs pour PyPI
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
     ],
-    keywords="smartcard emv payment",
-    python_requires=">=3.4",
-    packages=["emv", "emv.protocol", "emv.command"],
-    install_requires=[
+    keywords="smartcard emv payment",  # Mots-clés
+    python_requires=">=3.4",  # Version minimale de Python requise
+    packages=find_packages(where="."),  # Inclut automatiquement tous les sous-packages
+    install_requires=[  # Dépendances principales
         "pyscard==2.0.0",
         "pycountry==20.7.3",
         "terminaltables==3.1.0",
         "click==7.1.2",
     ],
-    entry_points={"console_scripts": {"emvtool=emv.command.client:run"}},
+    entry_points={  # Scripts de console
+        "console_scripts": [
+            "emvtool=emv.command.client:run",  # Exemple de commande console
+        ],
+    },
 )
